8d5b961200ae360374ac94b0850e0f728ba4731b
Ian Kenny <122339163@umail.ucc.ie>
2026-02-16 17:51:19 +0000

    camera capture now working

--- IoTDev/SmartCam/CameraAttempt5/main.c
+++ IoTDev/SmartCam/CameraAttempt5/main.c
@@ -11,15 +11,16 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/stat.h>
-
+#include <sys/wait.h>
 
 #define SYNC_PORT_OFFSET 1
-#define VIDEO_DEVICE "/dev/video0"
-#define OUTPUT_DIR   "/home/root/temp"
+#define OUTPUT_DIR "/home/root/temp"
 
 static volatile sig_atomic_t keep_running = 1;
 
-static uint64_t htobe64(uint64_t host_64) {
+/* ------------------- Utility ------------------- */
+
+static uint64_t htobe64_u(uint64_t host_64) {
 #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
     return ((uint64_t)htonl(host_64 & 0xFFFFFFFF) << 32) | htonl(host_64 >> 32);
 #else
@@ -27,126 +28,224 @@ static uint64_t htobe64(uint64_t host_64) {
 #endif
 }
 
-
-
-void handle_signal(int sig) { (void)sig; keep_running = 0; }
+void handle_signal(int sig) {
+    (void)sig;
+    keep_running = 0;
+}
 
 void get_iso_timestamp(char *buffer, size_t len) {
     struct timespec ts;
     struct tm tm;
     clock_gettime(CLOCK_REALTIME, &ts);
     gmtime_r(&ts.tv_sec, &tm);
-    snprintf(buffer, len, "%04d-%02d-%02dT%02d:%02d:%02d.%03ldZ",
-             tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday,
-             tm.tm_hour, tm.tm_min, tm.tm_sec, ts.tv_nsec/1000000);
+
+    snprintf(buffer, len,
+             "%04d-%02d-%02dT%02d:%02d:%02d.%03ldZ",
+             tm.tm_year + 1900,
+             tm.tm_mon + 1,
+             tm.tm_mday,
+             tm.tm_hour,
+             tm.tm_min,
+             tm.tm_sec,
+             ts.tv_nsec / 1000000);
 }
+
+int rand_range(int min, int max) {
+    if (min >= max)
+        return min;
+    return min + rand() % (max - min + 1);
+}
+
+/* ------------------- UDP JSON ------------------- */
 
 int send_udp_json(const char *host_ip, int port, const char *json) {
     int sock = socket(AF_INET, SOCK_DGRAM, 0);
-    if(sock < 0) return -1;
+    if (sock < 0)
+        return -1;
 
     struct sockaddr_in addr = {0};
     addr.sin_family = AF_INET;
     addr.sin_port = htons(port);
-    if(inet_pton(AF_INET, host_ip, &addr.sin_addr) != 1) { close(sock); return -1; }
 
-    ssize_t sent = sendto(sock, json, strlen(json), 0, (struct sockaddr*)&addr, sizeof(addr));
+    if (inet_pton(AF_INET, host_ip, &addr.sin_addr) != 1) {
+        close(sock);
+        return -1;
+    }
+
+    ssize_t sent = sendto(sock, json, strlen(json), 0,
+                          (struct sockaddr *)&addr, sizeof(addr));
+
     close(sock);
     return (sent < 0) ? -1 : 0;
 }
 
 void send_label(const char *host_ip, int port, const char *event) {
-    char ts[64], json[256];
+    char ts[64];
+    char json[256];
+
     get_iso_timestamp(ts, sizeof(ts));
-    snprintf(json, sizeof(json), "{ \"type\":\"LABEL\", \"event\":\"%s\", \"timestamp\":\"%s\" }\n", event, ts);
+
+    snprintf(json, sizeof(json),
+             "{ \"type\":\"LABEL\", \"event\":\"%s\", \"timestamp\":\"%s\" }\n",
+             event, ts);
+
     send_udp_json(host_ip, port, json);
 }
 
-int rand_range(int min, int max) { return (min>max)?min:(min + rand() % (max-min+1)); }
+/* ------------------- Camera Capture ------------------- */
 
-int capture_video(int duration_sec, const char *filename)
-{
-
+int capture_video(int duration_sec, const char *filename) {
     int camera_index = 1;
 
-    char cmd[512];
-    snprintf(cmd, sizeof(cmd),
-             "timeout %d gst-multi-camera-example -o %d && cp /opt/cam%d_vid.mp4 %s",
-             duration_sec, camera_index, camera_index, filename);
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork failed");
+        return -1;
+    }
 
+    if (pid == 0) {
+        /* Child process */
+        execlp("gst-multi-camera-example",
+               "gst-multi-camera-example",
+               "-o", "1",
+               (char *)NULL);
 
-    int ret = system(cmd);
+        perror("execlp failed");
+        exit(EXIT_FAILURE);
+    }
 
-    if (ret != 0) {
-        fprintf(stderr, "Error: Video capture failed (camera %d)\n", camera_index);
+    /* Parent */
+    printf("Camera running for %d seconds...\n", duration_sec);
+    sleep(duration_sec);
+
+    printf("Stopping camera (SIGINT)...\n");
+    kill(pid, SIGINT);
+
+    int status;
+    waitpid(pid, &status, 0);
+
+    if (!WIFEXITED(status)) {
+        fprintf(stderr, "Camera process did not exit cleanly\n");
+        return -1;
+    }
+
+    /* Copy output file */
+    char copy_cmd[512];
+    snprintf(copy_cmd, sizeof(copy_cmd),
+             "cp /opt/cam%d_vid.mp4 %s",
+             camera_index, filename);
+
+    if (system(copy_cmd) != 0) {
+        fprintf(stderr, "Failed to copy video file\n");
         return -1;
     }
 
     return 0;
 }
 
+/* ------------------- File Upload ------------------- */
+
 int upload_file(const char *host_ip, int port, const char *filename) {
     struct stat st;
-    if(stat(filename, &st)!=0) return -1;
-    FILE *fp = fopen(filename, "rb"); if(!fp) return -1;
+    if (stat(filename, &st) != 0)
+        return -1;
 
-    int sock = socket(AF_INET, SOCK_STREAM, 0); if(sock<0) goto fail;
-    struct sockaddr_in addr = {0}; addr.sin_family = AF_INET; addr.sin_port = htons(port);
-    if(inet_pton(AF_INET, host_ip, &addr.sin_addr)!=1) goto fail;
-    if(connect(sock,(struct sockaddr*)&addr,sizeof(addr))<0) goto fail;
+    FILE *fp = fopen(filename, "rb");
+    if (!fp)
+        return -1;
 
-    /* ---- HEADER ---- */
-    uint64_t size_net = htobe64((uint64_t)st.st_size);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0)
+        goto fail;
+
+    struct sockaddr_in addr = {0};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+
+    if (inet_pton(AF_INET, host_ip, &addr.sin_addr) != 1)
+        goto fail;
+
+    if (connect(sock, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+        goto fail;
+
+    uint64_t size_net = htobe64_u((uint64_t)st.st_size);
     uint16_t name_len = htons(strlen(filename));
-    send(sock,&size_net,sizeof(size_net),0);
-    send(sock,&name_len,sizeof(name_len),0);
-    send(sock,filename,strlen(filename),0);
 
-    /* ---- PAYLOAD ---- */
-    char buf[4096]; size_t n;
-    while((n=fread(buf,1,sizeof(buf),fp))>0) send(sock,buf,n,0);
+    send(sock, &size_net, sizeof(size_net), 0);
+    send(sock, &name_len, sizeof(name_len), 0);
+    send(sock, filename, strlen(filename), 0);
 
-    fclose(fp); close(sock); return 0;
+    char buf[4096];
+    size_t n;
+
+    while ((n = fread(buf, 1, sizeof(buf), fp)) > 0) {
+        send(sock, buf, n, 0);
+    }
+
+    fclose(fp);
+    close(sock);
+    return 0;
 
 fail:
-    if(sock>=0) close(sock); fclose(fp); return -1;
+    if (sock >= 0)
+        close(sock);
+    fclose(fp);
+    return -1;
 }
 
-int main(int argc,char*argv[]) {
-    if(argc!=7){
-        fprintf(stderr,"Usage: %s <host_ip> <host_port> <idle_min_m> <idle_max_m> <cap_min_s> <cap_max_s>\n",argv[0]);
+/* ------------------- Main ------------------- */
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 7) {
+        fprintf(stderr,
+                "Usage: %s <host_ip> <host_port> <idle_min_m> <idle_max_m> <cap_min_s> <cap_max_s>\n",
+                argv[0]);
         return 1;
     }
 
-    signal(SIGINT,handle_signal); signal(SIGTERM,handle_signal);
+    signal(SIGINT, handle_signal);
+    signal(SIGTERM, handle_signal);
 
-    const char *host_ip = argv[1]; int port = atoi(argv[2]); int sync_port = port+SYNC_PORT_OFFSET;
-    int idle_min = atoi(argv[3]); int idle_max = atoi(argv[4]);
-    int cap_min = atoi(argv[5]); int cap_max = atoi(argv[6]);
+    const char *host_ip = argv[1];
+    int port = (int)strtol(argv[2], NULL, 10);
+    int sync_port = port + SYNC_PORT_OFFSET;
+
+    int idle_min = (int)strtol(argv[3], NULL, 10);
+    int idle_max = (int)strtol(argv[4], NULL, 10);
+    int cap_min  = (int)strtol(argv[5], NULL, 10);
+    int cap_max  = (int)strtol(argv[6], NULL, 10);
+
     srand(time(NULL));
 
-    send_label(host_ip,sync_port,"START_SYNC");
+    send_label(host_ip, sync_port, "START_SYNC");
 
-    while(keep_running){
-        sleep(rand_range(idle_min,idle_max)*60);
+    while (keep_running) {
+
+        int idle_time = rand_range(idle_min, idle_max) * 60;
+        printf("Idling for %d seconds...\n", idle_time);
+        sleep(idle_time);
 
         char filename[256];
-        snprintf(filename,sizeof(filename), OUTPUT_DIR "/capture_%ld.mp4", time(NULL));
+        snprintf(filename, sizeof(filename),
+                 OUTPUT_DIR "/capture_%ld.mp4", time(NULL));
 
-        int cap_time = rand_range(cap_min,cap_max);
+        int cap_time = rand_range(cap_min, cap_max);
 
-        send_label(host_ip,sync_port,"CAMERA_START");
-        if(!capture_video(cap_time,filename)){
-            send_label(host_ip,sync_port,"CAMERA_FAILED");
+        send_label(host_ip, sync_port, "CAMERA_START");
+
+        if (capture_video(cap_time, filename) != 0) {
+            send_label(host_ip, sync_port, "CAMERA_FAILED");
             continue;
         }
-        send_label(host_ip,sync_port,"CAMERA_END");
 
-        send_label(host_ip,sync_port,"UPLOAD_START");
-        upload_file(host_ip,port,filename);
-        send_label(host_ip,sync_port,"UPLOAD_END");
+        send_label(host_ip, sync_port, "CAMERA_END");
+
+        send_label(host_ip, sync_port, "UPLOAD_START");
+        upload_file(host_ip, port, filename);
+        send_label(host_ip, sync_port, "UPLOAD_END");
     }
 
-    send_label(host_ip,sync_port,"SHUTDOWN");
+    send_label(host_ip, sync_port, "SHUTDOWN");
     return 0;
 }
